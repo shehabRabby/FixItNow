7832ddbb450375edaee92853272f9b21bbb80d08
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2026-07-17 17:08:40 +0600

    fix: adjust prisma 7 schema and config datasource format

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,21 +1,8 @@
-// import "dotenv/config";
-// import { defineConfig } from "prisma/config";
-
-// export default defineConfig({
-//   schema: "prisma/schema",
-//   migrations: {
-//     path: "prisma/migrations",
-//   },
-//   datasource: {
-//     url: process.env["DATABASE_URL"],
-//   },
-// });
-
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "./prisma/schema.prisma",
   datasource: {
-    url: (globalThis as any).process?.env?.DATABASE_URL || "",
+    url: ((globalThis as any).process?.env?.DATABASE_URL || "") as string,
   },
 });
