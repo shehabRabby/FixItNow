31d10c425e8bff5d88a62a667ecdd54e418fad8b
Md.Shehab Al Rabby <shehabrabby764@gmail.com>
2026-07-20 16:49:44 +0600

    feat: finalize vercel config with api/index.js

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "./prisma/schema.prisma",
+  schema: "./prisma/schema/schema.prisma",
   datasource: {
     url: ((globalThis as any).process?.env?.DATABASE_URL || "") as string,
   },
